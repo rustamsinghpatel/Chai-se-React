89a89b401871dd9d6b5c66edf4095c61a6aa088d
Rustam Patel <53313031+rustamsinghpatel@users.noreply.github.com>
2024-02-15 17:01:59 +0530

    basic

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,28 @@
+
+
+function App() {
+  return (
+
+    <h1>chai aur react || del krna sekha </h1>
+      
+    /*<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div> */
+    
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+  
+  return (
+    <>
+    <p>rustam</p>
+    <h1>chai aur React with vite || Rustam patel</h1>
+    </>
+  )
+}
+
+export default App
