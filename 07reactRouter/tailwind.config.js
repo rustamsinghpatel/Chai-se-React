54304cb2ecc30c57eaacc3250b71adcfd07f6ac2
Rustam Patel <53313031+rustamsinghpatel@users.noreply.github.com>
2025-05-09 11:14:40 +0530

    rauter

---
+++ 07reactRouter/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",              // Required for the base HTML file
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure all JS/JSX files are included
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
